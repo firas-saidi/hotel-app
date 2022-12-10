8e122c36245b8bcc3f09367bb62238903af44618
unknown <saidi.firas@outlook.com>
2022-12-10 20:42:02 +0100

    fix

--- src/componments/Main/Gallery/Gallery.js
+++ src/componments/Main/Gallery/Gallery.js
@@ -7,11 +7,11 @@ function Gallery() {
     <div className="swiper gallery-slider">
        <div className="swiper-wrapper">
           <img src="images/gallery-img-1.jpg" className="swiper-slide" alt=""/>
-          <img src="images/gallery-img-2.webp" className="swiper-slide" alt=""/>
-          <img src="images/gallery-img-3.webp" className="swiper-slide" alt=""/>
-          <img src="images/gallery-img-4.webp" className="swiper-slide" alt=""/>
-          <img src="images/gallery-img-5.webp" className="swiper-slide" alt=""/>
-          <img src="images/gallery-img-6.webp" className="swiper-slide" alt=""/>
+          <img src="images/gallery-img-2.jpg" className="swiper-slide" alt=""/>
+          <img src="images/gallery-img-3.jpg" className="swiper-slide" alt=""/>
+          <img src="images/gallery-img-4.jpg" className="swiper-slide" alt=""/>
+          <img src="images/gallery-img-5.jpg" className="swiper-slide" alt=""/>
+          <img src="images/gallery-img-6.jpg" className="swiper-slide" alt=""/>
        </div>
        <div className="swiper-pagination"></div>
     </div>
