e95f9b7ec09227a28e967125c3746c9d92291695
unknown <saidi.firas@outlook.com>
2022-12-29 01:45:09 +0100

    fix res

--- src/componments/Main/Main.js
+++ src/componments/Main/Main.js
@@ -5,6 +5,7 @@ import Gallery from "./Gallery/Gallery"
 import Contact from "./Contact/Contact"
 import Reviews from "./Reviews/Reviews"
 import ReservationS from "./ReservationS/ReservationS"
+import AboutS from "./AboutS/AboutS"
 
 function Main() {
   return (
